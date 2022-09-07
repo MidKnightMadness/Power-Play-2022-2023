c578163db74b8a2d72fc542a0784430b527e671c
redpotato <50560736+redpob@users.noreply.github.com>
2022-09-06 22:12:42 -0700

    mayhem

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.drive.MecanumDrive;
+
+@TeleOp(name="Main")
+public class Main extends OpMode {
+    MecanumDrive mecanum;
+
+    @Override
+    public void init() {
+        mecanum = new MecanumDrive(hardwareMap);
+    }
+
+    public void init_loop() {
+
+    }
+
+    public void start()
+    {
+
+    }
+
+    @Override
+    public void loop() {
+        mecanum.drive(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_y);
+        mecanum.telemetry(telemetry);
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/MecanumDrive.java
@@ -0,0 +1,57 @@
+package org.firstinspires.ftc.teamcode.drive;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class MecanumDrive {
+    DcMotorEx FRMotor;
+    DcMotorEx FLMotor;
+    DcMotorEx BRMotor;
+    DcMotorEx BLMotor;
+
+    private BNO055IMU imu;
+
+    public MecanumDrive(HardwareMap hardwareMap) {
+        FRMotor = hardwareMap.get(DcMotorEx.class, "FR");
+        FLMotor = hardwareMap.get(DcMotorEx.class, "FL");
+        BRMotor = hardwareMap.get(DcMotorEx.class, "BR");
+        BLMotor = hardwareMap.get(DcMotorEx.class, "BL");
+
+        FRMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        FLMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        BRMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        BLMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        //IMU
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json";
+        parameters.loggingEnabled      = true;
+        parameters.loggingTag          = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+    }
+
+    public void drive(double x, double y, double rotation) {
+        FRMotor.setPower(-x - y - rotation);
+        FLMotor.setPower(-x + y - rotation);
+        BRMotor.setPower(-x + y + rotation);
+        BLMotor.setPower(-x - y + rotation);
+    }
+
+    public void telemetry(Telemetry telemetry) {
+        telemetry.addData("FR Motor Position", FRMotor.getCurrentPosition());
+        telemetry.addData("FL Motor Position", FLMotor.getCurrentPosition());
+        telemetry.addData("BR Motor Position", BRMotor.getCurrentPosition());
+        telemetry.addData("BL Motor Position", BLMotor.getCurrentPosition());
+        telemetry.update();
+    }
+}
