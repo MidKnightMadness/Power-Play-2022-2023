c771d03f9316ad1dd5d9b26238fcb5bf53aec0aa
mike-wen1 <torchmaster123@gmail.com>
2022-10-26 17:04:50 -0700

    webcam preview

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/highlevel/GridSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/highlevel/GridSystem.java
@@ -1,33 +1,42 @@
 package org.firstinspires.ftc.teamcode.highlevel;
 
-import org.firstinspires.ftc.teamcode.drivetrain.Vector;
+import static org.firstinspires.ftc.teamcode.highlevel.Master.currentPosition;
+import static org.firstinspires.ftc.teamcode.highlevel.Master.odometryAlg;
+import static org.firstinspires.ftc.teamcode.manipulator.LinearSlides.ROOT_HEIGHT;
 
-enum Junction {
-    Ground,
-    Low,
-    Medium,
-    High,
-}
+import org.firstinspires.ftc.teamcode.drivetrain.Vector;
+import org.firstinspires.ftc.teamcode.highlevel.*;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.manipulator.DoubleReverse4Bar;
+import org.firstinspires.ftc.teamcode.manipulator.LinearSlides;
+import org.firstinspires.ftc.teamcode.manipulator.Turntable;
+import org.firstinspires.ftc.teamcode.odometry.TestingOdometryAlgorithm;
+import org.firstinspires.ftc.teamcode.drivetrain.*;
+
 
 public class GridSystem {
-    final Junction[][] junctionPositions = {
-            { Junction.Ground, Junction.Low, Junction.Ground, Junction.Low, Junction.Ground },
-            { Junction.Low, Junction.Medium, Junction.High, Junction.Medium, Junction.Low },
-            { Junction.Ground, Junction.High, Junction.Ground, Junction.High, Junction.Ground },
-            { Junction.Low, Junction.Medium, Junction.High, Junction.Medium, Junction.Low },
-            { Junction.Ground, Junction.Low, Junction.Ground, Junction.Low, Junction.Ground },
+    final static double[][] junctionHeights = { // Matrix format, A[i][j], ith column, jth row; inches
+            { 1.0,   14.5,  1.0,   14.5,  1.0 },
+
+            { 14.5,  24.5,  34.5,  24.5,  14.5 },
+
+            { 1.0,   34.5,  1.0,   34.5,  1.0 },
+
+            { 14.5,  24.5,  34.5,  24.5,  14.5 },
+
+            { 1.0,   14.5,  1.0,   14.5,  1.0 },
+
     };
 
-    final Integer[][] positions = {
-            { 0, 1, 0, 1, 0 } ,
-            { 1, 2, 3, 2, 1 },
-            { 0, 3, 0, 3, 0 },
-            { 1, 2, 3, 2, 1 },
-            { 0, 1, 0, 1, 0 },
-    };
+    public static double [] closestJunctionDisplacement(Vector position){ // Returns 3d displacement vector to be inputed to move methods
+        odometryAlg.updateOrientationAndLocation();
+        double [] auxillaryArrayToSpeedUpStuff = position.get(); // Use currentPosition if needed, but should pre-calculate during transition
 
-    Vector position = new Vector();
-    double rotation;
-
-
+        return new double [] {auxillaryArrayToSpeedUpStuff[0] - (22.75 * (Math.round(auxillaryArrayToSpeedUpStuff[0] / 22.75))),
+                auxillaryArrayToSpeedUpStuff[1] - (22.75 * (Math.round(auxillaryArrayToSpeedUpStuff[1] / 22.75))),
+                junctionHeights[(int) (6 - Math.round(auxillaryArrayToSpeedUpStuff[0] / 22.75))]
+                        [(int) (Math.round(auxillaryArrayToSpeedUpStuff[1] / 22.75) - 1)] - ROOT_HEIGHT};
+    }
 }
+
+
