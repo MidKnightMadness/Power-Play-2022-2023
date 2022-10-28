d7de4028c9dcccdc1bc844b620778cb2ee449a58
mike-wen1 <torchmaster123@gmail.com>
2022-10-28 12:29:54 -0700

    fixed fOD

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/highlevel/Master.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/highlevel/Master.java
@@ -1,6 +1,8 @@
 // Add import statements as more data is added
 // Auxillary (driver hub, control hub, OD, etc...)
 package org.firstinspires.ftc.teamcode.highlevel;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
@@ -22,12 +24,37 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 public class Master {
     public static HardwareMap hardwaremap;
 
+    // Robot dimensions
+    final static double leftSensorPos = 7.5; // Probably change this to correct notation
+    final static double rightSensorPos = 7.5;
+    final static double backSensorPos = 7.5;
+    final static double robotLength = 15; // Change this to good notation pls
+    final static double robotWidth = 15;
+
     // Controller
     public static Gamepad gamepad1;
     public static Gamepad gamepad2;
 
     // Driver hub and Vuforia
     public static Telemetry telemetry;
+    private BNO055IMU IMU;
+    /* Can extract:
+    1. Absolute Orientation (Euler Vector, 100Hz) Three axis orientation data based on a 360° sphere
+
+    2. Absolute Orientation (Quaterion, 100Hz) Four point quaternion output for more accurate data manipulation
+
+    3. Angular Velocity Vector (100Hz) Three axis of 'rotation speed' in rad/s
+
+    4. Acceleration Vector (100Hz) Three axis of acceleration (gravity + linear motion) in m/s^2
+
+    5. Magnetic Field Strength Vector (20Hz) Three axis of magnetic field sensing in micro Tesla (uT)
+
+    6. Linear Acceleration Vector (100Hz) Three axis of linear acceleration data (acceleration minus gravity) in m/s^2
+
+    7. Gravity Vector (100Hz) Three axis of gravitational acceleration (minus any movement) in m/s^2
+
+    8. Temperature (1Hz) Ambient temperature in degrees celsius
+     */
 
     // Odometry                             ________
     public static DcMotorEx encoder1; //   |   ==   |
@@ -39,6 +66,11 @@ public class Master {
     public static Vector orientation;
     public static Vector normalOrientation;
 
+    // Calibration
+    public static DistanceSensor leftSensor;
+    public static DistanceSensor rightSensor;
+    public static DistanceSensor backSensor;
+
     // Used to update encoder deltas
     public static double encoder1Reading;
     public static double encoder2Reading;
@@ -48,8 +80,6 @@ public class Master {
     // Probably write calibration method w/ tape and obj recognition
     public static Vector STARTING_POSITION;
     public static MecanumDrive drive;
-
-    // Drive motors
 
     // Manipulator
     GridSystem grid;
@@ -69,6 +99,10 @@ public class Master {
 
         manipulator1 = new LinearSlides();
         manipulator2 = new DoubleReverse4Bar(hardwaremap);
+
+        leftSensor = hardwaremap.get(DistanceSensor.class, "left calibration distance sensor");
+        rightSensor = hardwaremap.get(DistanceSensor.class, "right calibration distance sensor");
+        backSensor = hardwaremap.get(DistanceSensor.class, "back calibration distance sensor");
     }
 
     public static double invSqrt(double x) { // Use this for inverse square root ig, gotta tell judges we used some innovative bit shift algorithm originally in C++ or smth
