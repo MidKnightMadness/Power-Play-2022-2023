b1b885ba415aa0d87c718b1ab2ff3ea9dcc28836
mike-wen1 <torchmaster123@gmail.com>
2022-11-19 11:27:27 -0800

    Merge remote-tracking branch 'origin/Mayhem' into Mayhem
    # Conflicts:
    #	.gradle/7.4.2/fileHashes/fileHashes.lock

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/currentOpModes/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/currentOpModes/MainTeleOp.java
@@ -19,6 +19,8 @@ import org.firstinspires.ftc.teamcode.odometry.Odometry;
 import org.firstinspires.ftc.teamcode.odometry.TestingOdometryAlgorithm;
 import static org.firstinspires.ftc.teamcode.drivetrain.Vector.neg;
 import static org.firstinspires.ftc.teamcode.highlevel.Master.manipulator1;
+import static org.firstinspires.ftc.teamcode.highlevel.Master.turntableAngle;
+import static org.firstinspires.ftc.teamcode.manipulator.Turntable.tableMotor;
 //import static org.firstinspires.ftc.teamcode.highlevel.Master.tracking;
 
 import org.firstinspires.ftc.robotcore.external.android.AndroidAccelerometer;
@@ -78,9 +80,9 @@ public class MainTeleOp extends OpMode {
 
     @Override
     public void loop() {
-        timer.updateTime();
-        time = timer.getTime();
-        deltaTime = timer.getDeltaTime();
+//        timer.updateTime();
+//        time = timer.getTime();
+//        deltaTime = timer.getDeltaTime();
 
 //        Master.tickRate = 1 / (time - auxillary); // auxillary is previous time
 //        auxillaryList1[0] = currentPosition[0] - auxillaryList1[0];
@@ -88,25 +90,29 @@ public class MainTeleOp extends OpMode {
 //        Master.robotSpeed = lengthOf(auxillaryList1) / (time - auxillary);
 
         // DRIVER ASSIST
-        if (gamepad1.left_bumper && !lastPressedDriveMode) {
-            driveModeToggle = !driveModeToggle;
-        }
-        if (driveModeToggle) {
-            mecanum.fieldOrientatedDrive(gamepad1.left_stick_x, -gamepad1.left_stick_y, gamepad1.right_stick_x);
-            if (gamepad1.dpad_up) { mecanum.fieldOrientatedDrive(0, -1, 0); }
-            if (gamepad1.dpad_down) { mecanum.fieldOrientatedDrive(0, 1, 0); }
-            if (gamepad1.dpad_right) { mecanum.fieldOrientatedDrive(1, 0, 0); }
-            if (gamepad1.dpad_left) { mecanum.fieldOrientatedDrive(-1, 0, 0); }
-        } else {
-            mecanum.drive(gamepad1.left_stick_x, -gamepad1.left_stick_y, gamepad1.right_stick_x); // normal drive
-            if (gamepad1.dpad_up) { mecanum.drive(0, -1, 0); }
-            if (gamepad1.dpad_down) { mecanum.drive(0, 1, 0); }
-            if (gamepad1.dpad_right) { mecanum.drive(1, 0, 0); }
-            if (gamepad1.dpad_left) { mecanum.drive(-1, 0, 0); }
-        }
-        lastPressedDriveMode = gamepad1.left_bumper;
+//        if (gamepad1.left_bumper && !lastPressedDriveMode) {
+//            driveModeToggle = !driveModeToggle;
+//        }
+//        if (driveModeToggle) {
+//            mecanum.fieldOrientatedDrive(gamepad1.left_stick_x, -gamepad1.left_stick_y, gamepad1.right_stick_x);
+//            if (gamepad1.dpad_up) { mecanum.fieldOrientatedDrive(0, -1, 0); }
+//            if (gamepad1.dpad_down) { mecanum.fieldOrientatedDrive(0, 1, 0); }
+//            if (gamepad1.dpad_right) { mecanum.fieldOrientatedDrive(1, 0, 0); }
+//            if (gamepad1.dpad_left) { mecanum.fieldOrientatedDrive(-1, 0, 0); }
+//        } else {
+//            mecanum.drive(gamepad1.left_stick_x, -gamepad1.left_stick_y, gamepad1.right_stick_x); // normal drive
+//            if (gamepad1.dpad_up) { mecanum.drive(0, -1, 0); }
+//            if (gamepad1.dpad_down) { mecanum.drive(0, 1, 0); }
+//            if (gamepad1.dpad_right) { mecanum.drive(1, 0, 0); }
+//            if (gamepad1.dpad_left) { mecanum.drive(-1, 0, 0); }
+//        }
+//        lastPressedDriveMode = gamepad1.left_bumper;
 
         turntable.turnBy(gamepad2.left_stick_x / 5);
+
+        telemetry.addData("turntable motor reference", tableMotor);
+        telemetry.addData("turntable angle", turntableAngle);
+        telemetry.addData("turntable power", tableMotor.getPower());
 //        lift.pivotBy(gamepad2.left_stick_y / 5);
 //        turntable.turnBy(gamepad2.left_stick_x / 5);
 
@@ -115,12 +121,13 @@ public class MainTeleOp extends OpMode {
 
 
 
-        telemetry.addData("DRIVE MODE", driveModeToggle ? "FIELD ORIENTED": "NORMAL");
-        telemetry.addData("Left Stick X", gamepad1.left_stick_x);
-        telemetry.addData("Left Stick Y", gamepad1.left_stick_y);
-        telemetry.addData("Right Stick X", gamepad1.right_stick_x);
+//        telemetry.addData("DRIVE MODE", driveModeToggle ? "FIELD ORIENTED": "NORMAL");
+//        telemetry.addData("Left Stick X", gamepad1.left_stick_x);
+//        telemetry.addData("Left Stick Y", gamepad1.left_stick_y);
+//        telemetry.addData("Right Stick X", gamepad1.right_stick_x);
+
 //        odometry.telemetry(telemetry);
-        mecanum.telemetry(telemetry);
+//        mecanum.telemetry(telemetry);
 
         telemetry.addLine("\nTIMER");
         telemetry.addLine("DeltaTime " + deltaTime);
