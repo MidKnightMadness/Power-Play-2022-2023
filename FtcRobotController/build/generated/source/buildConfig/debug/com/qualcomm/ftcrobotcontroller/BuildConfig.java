0666ff416c26cbc293d9238a526716dfcd51f97d
mike-wen1 <torchmaster123@gmail.com>
2022-11-29 17:18:19 -0800

    Merge remote-tracking branch 'origin/Mayhem' into Mayhem
    # Conflicts:
    #	.gradle/7.4.2/fileHashes/fileHashes.lock

--- FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
+++ FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
@@ -8,5 +8,5 @@ public final class BuildConfig {
   public static final String LIBRARY_PACKAGE_NAME = "com.qualcomm.ftcrobotcontroller";
   public static final String BUILD_TYPE = "debug";
   // Field from default config.
-  public static final String APP_BUILD_TIME = "2022-11-19T15:35:14.666-0800";
+  public static final String APP_BUILD_TIME = "2022-11-29T16:58:38.119-0800";
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/TestClawTemporary.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/TestClawTemporary.java
@@ -4,23 +4,29 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.manipulator.Claw;
+import org.firstinspires.ftc.teamcode.manipulator.LinearSlides;
 
 @TeleOp(name = "Claw Tester")
 public class TestClawTemporary  extends OpMode {
     Claw claw;
 
+    LinearSlides slides;
+
     @Override
     public void init() {
         claw = new Claw(hardwareMap);
+
+        slides = new LinearSlides(hardwareMap);
     }
 
     @Override
     public void loop() {
         if(gamepad1.left_bumper){
-
             claw.openClaw();
         }else if(gamepad1.right_bumper){
             claw.closeClaw();
         }
+
+        LinearSlides.extensionMotor.setPower(-gamepad1.right_stick_y);
     }
 }
