d6325e4715998c26d3fb84210ddae947b90c6e4a
Kevin <kevinzhang882019@gmail.com>
2022-12-06 16:41:01 -0800

    controller smoothing

--- FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
+++ FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
@@ -8,5 +8,5 @@ public final class BuildConfig {
   public static final String LIBRARY_PACKAGE_NAME = "com.qualcomm.ftcrobotcontroller";
   public static final String BUILD_TYPE = "debug";
   // Field from default config.
-  public static final String APP_BUILD_TIME = "2022-12-02T17:23:12.449-0800";
+  public static final String APP_BUILD_TIME = "2022-12-06T16:21:04.316-0800";
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/currentOpModes/Commented.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.currentOpModes;
+
+public class Commented {
+    //        lastPressedDriveMode = gamepad1.left_bumper;
+
+//        turntable.turnBy(gamepad2.left_stick_x / 5);
+//
+//        claw.pivotBy(-gamepad2.right_stick_y);
+//
+//        telemetry.addData("turntable motor reference", tableMotor);
+//        telemetry.addData("turntable angle", turntableAngle);
+//        telemetry.addData("turntable power", tableMotor.getPower());
+//
+//        telemetry.addData("\n claw pivot servo reference", claw.rotationServo);
+//        telemetry.addData("claw pivot position", claw.rotationServo.getPosition());
+//
+//        telemetry.addData("\n claw servo reference", claw.servo);
+//        telemetry.addData("claw position", claw.servo.getPosition());
+
+//        lift.pivotBy(gamepad2.left_stick_y / 5);
+//        turntable.turnBy(gamepad2.left_stick_x / 5);
+
+//        handleManipulatorControls();
+
+
+
+
+    // Odometry Calibration
+//        odometry.updatePosition();
+//        telemetry.addLine(String.format("\nCoordinates: [%5.2f, %5.2f]", currentPosition [0], currentPosition [1]));
+//        telemetry.addData("Angle", odometry.getRotationDegrees());
+}
