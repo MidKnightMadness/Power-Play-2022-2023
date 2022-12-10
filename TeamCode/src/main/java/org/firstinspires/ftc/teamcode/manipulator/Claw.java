a469d1a56ad14955afdac5da7f7bb235387443de
redpotato <50560736+redpob@users.noreply.github.com>
2022-12-09 17:34:23 -0800

    Commit Massage

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/manipulator/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/manipulator/Claw.java
@@ -5,40 +5,29 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.ServoController;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.common.Timer;
 
 public class Claw {
     public Timer calibrationTimer;
 
-
-    // Alternative
     public Servo servo;
-    public Servo rotationServo;
-    public Servo rotationServo2;
-
-
-    private ServoController servoController;
-//    private ServoController rotationServoController;
-//        private ServoController rotationServoController2;
+    public CRServo rotationServo;
+    public CRServo rotationServo2;
 
     double OPEN = 0.425;
     double CLOSED = 0.750;
-
-    private double downPosition = 0;
-    private double upPosition = 0;
-
-    public boolean open = false;
 
     public Claw(HardwareMap hardwareMap) {
         calibrationTimer = new Timer();
 
          servo = hardwareMap.get(Servo.class, "Claw");
 
-         rotationServo = hardwareMap.get(Servo.class, "CP");
-        rotationServo.setDirection(Servo.Direction.FORWARD);
+         rotationServo = hardwareMap.get(CRServo.class, "CP");
+         rotationServo.setDirection(CRServo.Direction.FORWARD);
 
-         rotationServo2 = hardwareMap.get(Servo.class, "CP2");
-         rotationServo2.setDirection(Servo.Direction.REVERSE);
+         rotationServo2 = hardwareMap.get(CRServo.class, "CP2");
+         rotationServo2.setDirection(CRServo.Direction.REVERSE);
 
 
 
@@ -60,12 +49,14 @@ public class Claw {
 
     public void openClaw() {
         servo.setPosition(OPEN);
-        boolean open = true;
     }
     
     public void closeClaw() {
         servo.setPosition(CLOSED);
-        boolean open = false;
+    }
+
+    public void rotateClaw(double power) {
+        rotationServo.setPower(power);
     }
 
     public void waitForOpenClaw() {
@@ -73,16 +64,10 @@ public class Claw {
         while (servo.getPosition() < servo.MAX_POSITION) { }
     }
 
-    public void pivotBy(double ticks){
-        rotationServo.setPosition(rotationServo.getPosition() + ticks / 2);
-
-        rotationServo2.setPosition(rotationServo.getPosition() + ticks / 2);
-
-    }
-
-    public static final double WRIST_RATIO = 0.5 / Math.PI;  // Ticks per radian
-    public void pivotTo(double angle){ // Radians
-
+    public void telemetry(Telemetry telemetry) {
+        telemetry.addLine("\nCLAW");
+        telemetry.addLine(String.format("Claw Servo Position: %f", servo.getPosition()));
+        telemetry.addLine(String.format("Claw Rotation Servo Power: %f %f", rotationServo.getPower(), rotationServo2.getPower()));
     }
 
 }
