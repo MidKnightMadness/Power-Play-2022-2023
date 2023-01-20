c29140346a8acb397b7fbb3742386b3737b21b4d
mike-wen1 <torchmaster123@gmail.com>
2023-01-19 19:14:04 -0800

    Merge remote-tracking branch 'origin/Mayhem' into Mayhem
    # Conflicts:
    #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/currentOpModes/MainTeleOp.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/SingleMotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/SingleMotorTest.java
@@ -18,6 +18,7 @@ public class SingleMotorTest extends OpMode {
         motor = hardwareMap.get(DcMotorEx.class, "motor");
         motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         motor.setDirection(DcMotorSimple.Direction.REVERSE);
+        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 //        motor.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, new PIDFCoefficients(1.0, 0.0, 0.0, 0.0));
         zeroPosition = motor.getCurrentPosition();
     }
@@ -30,13 +31,13 @@ public class SingleMotorTest extends OpMode {
     @Override
     public void loop() {
 //        motorInput = lastInput * hysteresisCoefficient + (1 - hysteresisCoefficient) * gamepad1.right_stick_x;
-        motor.setPower(gamepad1.right_stick_y);
+//        motor.setPower(gamepad1.right_stick_y);
 
-//        if(motor.getCurrentPosition() > zeroPosition){
-//            motor.setPower(gamepad1.right_stick_y);
-//        }else{
-//            motor.setPower(Math.max(gamepad1.right_stick_y, 0));
-//        }
+        if(motor.getCurrentPosition() > zeroPosition){
+            motor.setPower(gamepad1.right_stick_y);
+        }else{
+            motor.setPower(Math.max(gamepad1.right_stick_y, 0));
+        }
 //        lastInput = motorInput;
 
         if(gamepad1.x){
