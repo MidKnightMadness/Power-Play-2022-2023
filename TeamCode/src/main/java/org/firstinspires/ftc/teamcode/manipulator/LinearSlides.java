dccf5e3a21aff46b30d367e73de052850b47c84a
mike-wen1 <torchmaster123@gmail.com>
2023-02-06 19:04:21 -0800

    Commit Message

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/manipulator/LinearSlides.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/manipulator/LinearSlides.java
@@ -158,8 +158,8 @@ public class LinearSlides {
     double seesawPowerProfile = 0.0;
     public void pivotBy(double power) {
         brake = 0.0002 * (seesawExtensionLength / 2) * Math.cos(seesawAngle);
-        seesawPowerProfile = (1 / (1 + Math.exp(seesawAngle * seesawAngle + Math.PI / 2)))
-                * (seesawAngle * seesawAngle + Math.PI / 2)
+        seesawPowerProfile = (1 / (1 + Math.exp(seesawAngle * seesawAngle * seesawAngle + Math.PI / 2)))
+                * (seesawAngle * seesawAngle * seesawAngle + Math.PI / 2)
                 / 0.27027;
 
         if(Math.abs(power) < 0.1){
